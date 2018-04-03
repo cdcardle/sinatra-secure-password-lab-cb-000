ddaf33f895b654d549e41d335072e2a2ca8ddc37
Chris Cardle <cdcardle@gmail.com>
2018-04-03 18:31:09 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403181246) do
+ActiveRecord::Schema.define(version: 20180403173850) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.string "balance",         default: "0.00"
   end
 
 end
