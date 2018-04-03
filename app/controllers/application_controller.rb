0701b704e3340bed6ee199c29f5ebfe5595c79ad
Chris Cardle <cdcardle@gmail.com>
2018-04-03 18:08:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(:username => params[:username])
 
     if user
-      redirect "/account"
+      erb :account
     else
       redirect "/failure"
     end
